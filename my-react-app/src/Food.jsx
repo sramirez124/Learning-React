b685a3a9308e9139f1decf6c2c8b44daeefa9aa6
sramirez124 <forwardemails124@gmail.com>
2024-11-17 16:13:40 -0600

    Basic React App

---
+++ my-react-app/src/Food.jsx
@@ -0,0 +1,16 @@
+function Food() {
+
+    const food1 = "Pizza"
+    const food2 = "Burger"
+    
+    return (
+        <div>
+            <ul>
+                <li>{food1}</li>
+                <li>{food2}</li>
+            </ul>
+        </div>
+    )
+} 
+
+export default Food
--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -1,5 +1,16 @@
 function Header() {
-
+    return(
+        <header>
+            <h1>My React App</h1>
+            <nav>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Profile</a></li>
+                    <li><a href="">About</a></li>    
+                </ul>
+            </nav>
+        </header>
+    )
 }
 
 export default Header
