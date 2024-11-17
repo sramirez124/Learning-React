2c243b4f354ee3ba4966b6c2f372ce8fbf3b57d4
sramirez124 <forwardemails124@gmail.com>
2024-11-17 16:46:13 -0600

    basic card components start

---
+++ Card Components/card-components/src/Card.jsx
@@ -0,0 +1,13 @@
+
+
+function Card(){
+    return(
+        <div className="card">
+            <img src="https://via.placeholder.com/150" alt="Profile Picture" />
+            <h2>Steven Ramirez</h2>
+            <p>Fullstack Developer | Game Designer</p>
+        </div>
+    )
+}
+
+export default Card
