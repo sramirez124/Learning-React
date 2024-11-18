7774a7ab1008714448c3406d95dacf09c88bc0d6
sramirez124 <forwardemails124@gmail.com>
2024-11-17 18:07:51 -0600

    Finished card component

--- Card Components/card-components/src/Card.jsx
+++ Card Components/card-components/src/Card.jsx
@@ -1,11 +1,9 @@
-
-
 function Card(){
     return(
         <div className="card">
-            <img src="https://via.placeholder.com/150" alt="Profile Picture" />
-            <h2>Steven Ramirez</h2>
-            <p>Fullstack Developer | Game Designer</p>
+            <img className="card-image" src="https://via.placeholder.com/150" alt="Profile Picture" />
+            <h2 className="card-name">Steven Ramirez</h2>
+            <p className="card-title">Fullstack Developer | Game Designer</p>
         </div>
     )
 }
