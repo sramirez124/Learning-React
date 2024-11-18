e4e8e9c33b79d2d44407cfd50f61a1241e402ba4
sramirez124 <forwardemails124@gmail.com>
2024-11-17 20:42:08 -0600

    refactoring List.jsx

--- render-list/render-list/src/List.jsx
+++ render-list/render-list/src/List.jsx
@@ -1,10 +1,12 @@
 function List () {
-    const fruits = ['apple', 'banana', 'cherry']
+    const fruits = [{name: 'apple', calories: 95},
+                   {name:'banana', calories: 105},
+                   {name:'cherry' , calories: 120}]
 
     // Take the original array and return a new array
-    const listItems = fruits.map((fruit) => <li>{fruit}</li>)
+    const listItems = fruits.map((fruit) => <li key={fruit.name}>{fruit.name}</li>)
 
-    return (fruits);
+    return (<ul>{listItems}</ul>);
 }
 
 export default List
