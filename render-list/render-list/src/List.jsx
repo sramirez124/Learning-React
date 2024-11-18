67fec99f4e19723e14a2e4e77292a9727ff27bd9
sramirez124 <forwardemails124@gmail.com>
2024-11-17 20:47:18 -0600

    added a sort function

--- render-list/render-list/src/List.jsx
+++ render-list/render-list/src/List.jsx
@@ -3,8 +3,16 @@ function List () {
                    {id: 2, name:'banana', calories: 105},
                    {id: 3, name:'cherry' , calories: 120}]
 
+    // Alphabetical Sort
+    // fruits.sort((a, b) => a.name.localeCompare(b.name)) 
+
+    // Reverse Sort
+    // fruits.sort((a, b) => b.name.localeCompare(a.name))
+
     // Take the original array and return a new array
-    const listItems = fruits.map((fruit) => <li key={fruit.name}>{fruit.name}</li>)
+    const listItems = fruits.map((fruit) => <li key={fruit.id}>
+                                {fruit.name}: &nbsp;
+                                <b>{fruit.calories}</b></li>)
 
     return (<ul>{listItems}</ul>);
 }
